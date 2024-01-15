247eb6661cd16c360ea3168b99ad977dec75010b
MathOlympian <iyinevb@gmail.com>
2024-01-15 15:05:11 +0100

    first commit

---
+++ values.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main (){
+
+	// strings can be added with +
+	fmt.Println("go" + "lang") 
+	
+	// Integers and floats
+	fmt.Println("1 + 1 =", 1 + 1)
+	fmt.Println("7.0/3.0= ", 7.0/3.0)
+
+	// Booleans can be printed
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
